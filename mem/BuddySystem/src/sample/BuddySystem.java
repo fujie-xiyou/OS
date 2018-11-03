649de5e2e10142dbcd80a4d7800cf63e649141e5
fujie <fujie@xiyoulinux.org>
2018-11-03 18:40:35 +0800

    优化封装

--- mem/BuddySystem/src/sample/BuddySystem.java
+++ mem/BuddySystem/src/sample/BuddySystem.java
@@ -1,18 +1,19 @@
 package sample;
 
-import sample.MemBlock;
-import sample.MyMath;
-
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 
 public class BuddySystem {
-    private int N = 2;
-    private boolean[] map = new boolean[1 + 2 * (int)Math.pow(2,N)];
-    private List<MemBlock> usedMem = new LinkedList<>();
-    private List<MemBlock>[] free_area = new List[N + 1];
-
+    private int N;
+    private boolean[] map;
+    private List<MemBlock> usedMem;
+    private List<MemBlock>[] free_area;
+    public BuddySystem(int N){
+        this.N = N;
+        map = new boolean[1 + 2 * (int)Math.pow(2,N)];
+        usedMem = new LinkedList<>();
+        free_area = new List[N + 1];
+    }
     public int getN() {
         return N;
     }
@@ -27,68 +28,6 @@ public class BuddySystem {
 
     public List<MemBlock>[] getFree_area() {
         return free_area;
-    }
-
-    public static void main(String[] args) {
-        BuddySystem buddySystem = new BuddySystem();
-        List<MemBlock>[] free_area = buddySystem.getFree_area();
-        int N = buddySystem.getN();
-        List<MemBlock> usedMem = buddySystem.getUsedMem();
-        for (int i = 0; i < free_area.length; i++) {
-            free_area[i] = new LinkedList<>();
-        }
-        free_area[N].add(new MemBlock(2));
-        for (List list : free_area) {
-            System.out.print(list.size() + "  ");
-        }
-        Scanner s = new Scanner(System.in);
-
-        //输入数字以分配内存
-        while (true) {
-            long t = s.nextLong();
-            if (t == -1) break;
-            int n = (int) Math.ceil(MyMath.log2(t));
-            if(n > N) {
-                System.err.println("超过最大内存!");
-                continue;
-            }
-            buddySystem.f(n, n,t);
-
-            for(int i = 0; i <= N; i++){
-                System.out.print(i + "  ");
-            }
-            System.out.println();
-            for (List list : free_area) {
-                System.out.print(list.size() + "  ");
-            }
-            System.out.println("\n已分配的内存信息如下:");
-            for (MemBlock block : usedMem){
-                System.out.printf("%-4d",block.getIndex());
-            }
-            System.out.println();
-            for (MemBlock block : usedMem){
-                System.out.printf("%-4d",block.getNeedSize());
-            }
-
-        }
-
-        //输入序号以释放内存
-        while (true){
-            int index = s.nextInt();
-            if (index == -1) break;
-            if(usedMem.size() == 0 || index > usedMem.size()) {
-                System.err.println("没有这块内存！");
-                continue;
-            }
-            buddySystem.g(usedMem.get(index));
-
-            for (List list : free_area) {
-                System.out.print(list.size() + "  ");
-
-            }
-
-        }
-        s.close();
     }
 
     // 分配算法
--- mem/BuddySystem/src/sample/Test.java
+++ mem/BuddySystem/src/sample/Test.java
@@ -1,9 +1,69 @@
 package sample;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Test {
-    public static void main(String[] args){
-        for(int i = 1; i < 10; i++){
-            System.out.println("log2 "+i+" = "+MyMath.log2(i));
+    public static void main(String[] args) {
+        BuddySystem buddySystem = new BuddySystem(2);
+        List<MemBlock>[] free_area = buddySystem.getFree_area();
+        int N = buddySystem.getN();
+        List<MemBlock> usedMem = buddySystem.getUsedMem();
+        for (int i = 0; i < free_area.length; i++) {
+            free_area[i] = new LinkedList<>();
         }
+        free_area[N].add(new MemBlock(2));
+        for (List list : free_area) {
+            System.out.print(list.size() + "  ");
+        }
+        Scanner s = new Scanner(System.in);
+
+        //输入数字以分配内存
+        while (true) {
+            long t = s.nextLong();
+            if (t == -1) break;
+            int n = (int) Math.ceil(MyMath.log2(t));
+            if(n > N) {
+                System.err.println("超过最大内存!");
+                continue;
+            }
+            buddySystem.f(n, n,t);
+
+            for(int i = 0; i <= N; i++){
+                System.out.print(i + "  ");
+            }
+            System.out.println();
+            for (List list : free_area) {
+                System.out.print(list.size() + "  ");
+            }
+            System.out.println("\n已分配的内存信息如下:");
+            for (MemBlock block : usedMem){
+                System.out.printf("%-4d",block.getIndex());
+            }
+            System.out.println();
+            for (MemBlock block : usedMem){
+                System.out.printf("%-4d",block.getNeedSize());
+            }
+
+        }
+
+        //输入序号以释放内存
+        while (true){
+            int index = s.nextInt();
+            if (index == -1) break;
+            if(usedMem.size() == 0 || index > usedMem.size()) {
+                System.err.println("没有这块内存！");
+                continue;
+            }
+            buddySystem.g(usedMem.get(index));
+
+            for (List list : free_area) {
+                System.out.print(list.size() + "  ");
+
+            }
+
+        }
+        s.close();
     }
 }
